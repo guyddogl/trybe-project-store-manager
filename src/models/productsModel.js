8e8cbc4018d99ecc8ebbd95e586b09536a50af3d
Guyddo <guyddogl@gmail.com>
2022-11-11 16:20:36 -0300

    Create productsModel

---
+++ src/models/productsModel.js
@@ -0,0 +1,18 @@
+const connection = require('../db/connection');
+
+const getAllProducts = async () => {
+  const query = 'SELECT * FROM products';
+  const [products] = await connection.execute(query);
+  return products;
+};
+
+const getProductById = async (id) => {
+  const query = 'SELECT * FROM products WHERE id = ?';
+  const [product] = await connection.execute(query, [id]);
+  return product;
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
