c61b90bfd0a9cee18d6122451770b55d97710a38
Guyddo <guyddogl@gmail.com>
2022-11-11 16:24:31 -0300

    Create route to add new product

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/:id', productsController.getProductById);
 router.get('/', productsController.getAllProducts);
+router.post('/', productsController.addNewProduct);
 
 module.exports = router;
