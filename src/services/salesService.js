eed700f326c59001345980b14b73979f75ce7990
Guyddo <guyddogl@gmail.com>
2022-11-12 10:35:19 -0300

    Create updateSale service

--- src/services/salesService.js
+++ src/services/salesService.js
@@ -16,6 +16,13 @@ const addNewSale = async (sale) => {
   return { status: 201, id };
 };
 
+const updateSale = async (id, sale) => {
+  const findSale = await salesModel.getSaleById(id);
+  if (findSale.length === 0) return { status: 404, message: 'Sale not found' };
+  sale.map((e) => salesModel.updateSale(id, e));
+  return { status: 200 };
+};
+
 const deleteSale = async (id) => {
   await salesModel.deleteSale(id);
   return { status: 204 };
@@ -25,5 +32,6 @@ module.exports = {
   getAllSales,
   getSaleById,
   addNewSale,
+  updateSale,
   deleteSale,
 };
