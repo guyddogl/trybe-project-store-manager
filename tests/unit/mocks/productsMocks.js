f693120d096157f4cbece1b68fe3889980912c80
Guyddo <guyddogl@gmail.com>
2022-11-12 12:25:16 -0300

    Create productsMocks

---
+++ tests/unit/mocks/productsMocks.js
@@ -0,0 +1,32 @@
+const allProducts = [
+  {
+    "id": 1,
+    "name": "Lâminas do Caos"
+  },
+  {
+    "id": 2,
+    "name": "Machado Leviatã"
+  },
+];
+
+const productFound = {
+  "id": 2,
+  "name": "Machado Leviatã"
+};
+
+const productNotFoundMessage = { "message": "Product not found" };
+
+const newProduct = 'Arco de Garra';
+
+const productAdded = {
+  "id": 3,
+  "name": "Arco de Garra"
+};
+
+module.exports = {
+  allProducts,
+  productFound,
+  productNotFoundMessage,
+  newProduct,
+  productAdded,
+};
