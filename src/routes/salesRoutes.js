3edbd5a5307d14647706b8f461fb35b89caf3d9a
Guyddo <guyddogl@gmail.com>
2022-11-12 10:03:12 -0300

    Create deleteSales route

--- src/routes/salesRoutes.js
+++ src/routes/salesRoutes.js
@@ -2,13 +2,18 @@ const express = require('express');
 const salesController = require('../controllers/salesController');
 const { validateProductId,
   validateProductQuantity,
-  validateIfProductExist } = require('../middlewares/validateProductsSale');
+  validateIfProductExist,
+  validateIfSaleExist } = require('../middlewares/validateProductsSale');
 
 const router = express.Router();
 
 router.get('/:id', salesController.getSaleById);
+
 router.get('/', salesController.getAllSales);
-router.post('/', validateProductId, validateIfProductExist, 
-validateProductQuantity, salesController.addNewSale);
+
+router.post('/', validateProductId, validateIfProductExist, validateProductQuantity,
+  salesController.addNewSale);
+
+router.delete('/:id', validateIfSaleExist, salesController.deleteSale);
 
 module.exports = router;
