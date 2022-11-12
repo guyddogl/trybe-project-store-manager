06994530a61b6a60756cb712b594d7bb65b0e2c4
Guyddo <guyddogl@gmail.com>
2022-11-12 17:00:41 -0300

    Refactor: product is no longer an array

--- src/middlewares/validateProductToDelete.js
+++ src/middlewares/validateProductToDelete.js
@@ -3,7 +3,7 @@ const productsModel = require('../models/productsModel');
 const validateProductToDelete = async (req, res, next) => {
   const { id } = req.params;
   const product = await productsModel.getProductById(id);
-  if (product.length === 0) {
+  if (!product) {
     return res.status(404).json({ message: 'Product not found' });
   }
   next();
