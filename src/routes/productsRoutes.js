bcf087a6cfce652fcf720ec11a834682837459fd
Guyddo <guyddogl@gmail.com>
2022-11-11 21:16:58 -0300

    Create deleteProduct route

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const productsController = require('../controllers/productsController');
 const { validateProductName } = require('../middlewares/validateProductName');
+const { validateProductToDelete } = require('../middlewares/validateProductToDelete');
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.get('/:id', productsController.getProductById);
 router.get('/', productsController.getAllProducts);
 router.post('/', validateProductName, productsController.addNewProduct);
 router.put('/:id', validateProductName, productsController.updateProduct);
+router.delete('/:id', validateProductToDelete, productsController.deleteProduct);
 
 module.exports = router;
