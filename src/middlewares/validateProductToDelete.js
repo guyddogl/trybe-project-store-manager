d4fb83aa90c297ab79745636652afc283fd55dd8
Guyddo <guyddogl@gmail.com>
2022-11-12 10:01:36 -0300

    Remove console.log

--- src/middlewares/validateProductToDelete.js
+++ src/middlewares/validateProductToDelete.js
@@ -3,7 +3,6 @@ const productsModel = require('../models/productsModel');
 const validateProductToDelete = async (req, res, next) => {
   const { id } = req.params;
   const product = await productsModel.getProductById(id);
-  console.log(product);
   if (product.length === 0) {
     return res.status(404).json({ message: 'Product not found' });
   }
